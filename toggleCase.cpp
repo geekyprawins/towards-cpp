097f27ebc12548e81115c67321a84b1d4e50ccc1
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-07-05 17:49:34 +0530

    string challenge

---
+++ toggleCase.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << str << endl;
+
+    for (int i = 0; i < str.length(); i++)
+    { // to upper case
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] -= 32;
+        }
+        // to lower case
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] += 32;
+        }
+        else
+            continue;
+    }
+    cout << "Toggled String is " << str << endl;
+    return 0;
+}
